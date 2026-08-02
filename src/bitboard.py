08c8bd0f501887fbfb921220b563be9199a12a04
hjabbot <42661345+hjabbot@users.noreply.github.com>
2026-08-02 00:47:09 -0600

    Extended Bitboard functionality with str, repr, add, sub, lsb, msb, count, shift

--- src/bitboard.py
+++ src/bitboard.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Bitboard(int):
 
     def __str__(self):
@@ -36,6 +39,66 @@ class Bitboard(int):
             output += row + '\n'
         return output
 
+    def __repr__(self):
+        return str(self)
+
+    def __add__(self, other):
+        assert type(self) == type(other), "Can only add two Bitboard objects together"
+        # Combine bitboards together as union
+        return Bitboard(self | other)
+
+    def __sub__(self, other):
+        assert type(self) == type(other), "Can only subtract two Bitboard objects together"
+        # Subtract any overlap from the first
+        # int(self) to avoid recursion calling self.__sub__
+        return Bitboard(int(self) - (self & other))
+
+    @property
+    def least_significant_bit(self):
+        return Bitboard(np.log2(self & -self))
+
+    @property
+    def most_significant_bit(self):
+        return Bitboard(np.log2(self))
+
+    @property
+    def count(self):
+        """
+        Counts number of bits = 1
+
+        Returns:
+            int: Number of positive bits
+        """
+        bits = int(self)
+        n_bits = 0
+        while bits > 0:
+            if bits % 1:
+                n_bits += 1
+            bits = bits >> 1
+        return n_bits
+
+    def shift(self, amount):
+        """
+        Shifts the bits by an amount
+        Positive is <<
+        Negative is >>
+
+        Args:
+            amount (int): How much to shift bitboard by
+
+        Returns:
+            Bitboard: New shifted bitboard
+        """
+        if amount > 0:
+            return Bitboard(self << amount)
+        else:
+            return Bitboard(self >> -amount)
+
+        
+    
 if __name__ == "__main__":
-    bb = Bitboard(1037 << 4 * 8)
+    bb = Bitboard(32)
+    # bb = Bitboard(1037 << 4 * 8)
     print(bb)
+    print(bb.least_significant_bit)
+    print(bb.most_significant_bit)
